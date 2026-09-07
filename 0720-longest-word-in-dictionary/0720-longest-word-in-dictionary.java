2bfc3054776f7eef6721e11825b8085d224f6d38
Gowtham Medapati <medapatigowtham9@gmail.com>
2026-09-07 23:36:59 +0530

    Time: 35 ms (11.57%), Space: 47 MB (38.66%) - LeetHub

---
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String longestWord(String[] words) {
+        java.util.HashSet<String> set = new java.util.HashSet<>();
+
+        for (String word : words) {
+            set.add(word);
+        }
+
+        String ans = "";
+
+        for (String word : words) {
+            boolean valid = true;
+
+            for (int i = 1; i < word.length(); i++) {
+                if (!set.contains(word.substring(0, i))) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid) {
+                if (word.length() > ans.length() ||
+                    (word.length() == ans.length() && word.compareTo(ans) < 0)) {
+                    ans = word;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
